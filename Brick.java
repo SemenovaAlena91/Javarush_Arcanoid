fe7bc8b4be92712f2dbdf01c05d0f6de1ca84aac
Alena Semenova <semenova.alena91@mail.ru>
2021-05-21 21:42:41 +0300

    added vars speed, direction for stand and getters for it

--- Brick.java
+++ Brick.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task24.task2413;
 
 public class Brick extends BaseObject{
+
+
     public Brick(double x, double y){
         super(x,y,3);
     }
--- Stand.java
+++ Stand.java
@@ -1,6 +1,24 @@
 package com.javarush.task.task24.task2413;
 
+//подставка, от которой отскакивает шарик
 public class Stand extends BaseObject{
+    private double speed; //скорость шарика
+    private double direction; //направление движения по оси x: 1 - вправо, -1 - влево, 0 - начальное значение, стоим на месте
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
     public Stand(double x, double y, double radius){
         super(x,y,radius);
     }
